6b0560c79dc66279d5543599822b3dd6ad30e4c2
GeeJay1983 <gertjan.jaarsma+git@gmail.com>
2014-02-08 16:37:08 +0100

    BookStoreManager Module 2
    Added a controller for editing books.

---
+++ app/services/Model.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = 'model';
+
+    angular.module('app').factory(serviceId, [model]);
+
+    function model() {
+        return {
+            Book: Book
+        };
+    }
+
+    function Book() {
+        this.Id = 0;
+        this.title = "";
+        this.author = "";
+        this.price = 0;
+    }
+})();
